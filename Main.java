55d49369b60ab78e2d1ab6e6b2b68599211d4f3d
Adam Vinch <adamvinch@gmail.com>
2024-09-23 11:57:23 -0400

    Start algorithm to find starting puzzle

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int[][] solvedBoard = Sudoku.parseBoardFromFile("sudoku.txt");
-        int[][] startingBoard = SudokuSolver.findStart(solvedBoard);
+        int[][] startingBoard = SudokuSolver.findStart(solvedBoard, 81);
 
         Sudoku sudoku = new Sudoku(solvedBoard);
         sudoku.printBoard();
--- SudokuSolver.java
+++ SudokuSolver.java
@@ -1,9 +1,24 @@
+import java.util.Random;
+
 public class SudokuSolver {
+    
+    public static final int STARTING_HINTS = 30;
+
+
     public static int[][] solveBoard(int[][] board) {
         return null;
     }
 
-    public static int[][] findStart(int[][] solvedBoard) {
+    public static int[][] findStart(int[][] board, int totalTiles) {
+        if (totalTiles == STARTING_HINTS) {
+            return board;
+        }
+    
+        Random random = new Random();
+        int row = random.nextInt(10);
+        int column = random.nextInt(10);
+         
+
         return null;
     }
 }
